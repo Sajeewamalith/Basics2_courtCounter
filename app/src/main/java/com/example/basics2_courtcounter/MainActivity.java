685953c9ddb29d42f29f58a0370ad7b5425d6437
sajeewa malith <sajeewamalith0219@gmail.com>
2022-04-26 18:01:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basics2_courtcounter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.basics2_courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
