0a95d3bd4913a89dab738aeb21353ce6ba8c3f3d
sajeewaMalith <sajeewamalith0219@gmail.com>
2022-05-14 00:06:58 +0530

    change actionBar colour and fix UI

--- app/src/main/java/com/example/basics2_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/basics2_courtcounter/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.basics2_courtcounter;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // change actionBar colour
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#FF6700"));
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+
     }
 }
