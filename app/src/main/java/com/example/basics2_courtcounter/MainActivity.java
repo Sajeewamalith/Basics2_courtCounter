bd8eefd232da696081a188486bbd9fdeb35b5912
sajeewaMalith <sajeewamalith0219@gmail.com>
2022-05-14 11:52:59 +0530

    java code teamA,teamB and reset button

--- app/src/main/java/com/example/basics2_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/basics2_courtcounter/MainActivity.java
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    int quantity = 0;
+    int quantity_b = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // change actionBar colour
+        /**
+         *  change actionBar colour
+         */
         // Define ActionBar object
         ActionBar actionBar;
         actionBar = getSupportActionBar();
@@ -28,5 +33,57 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setBackgroundDrawable(colorDrawable);
 
 
+    }
+
+    /**
+     * Team A
+     */
+    private void displayA(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.a_quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    public void point3(View view) {
+        quantity= quantity+3;
+        displayA(quantity);
+    }
+    public void point2(View view) {
+        quantity= quantity+2;
+        displayA(quantity);
+    }
+    public void point1(View view) {
+        quantity= quantity+1;
+        displayA(quantity);
+    }
+
+    /**
+     * Team B
+     */
+    private void displayB(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.b_quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    public void point3B(View view) {
+        quantity_b= quantity_b+3;
+        displayB(quantity_b);
+    }
+    public void point2B(View view) {
+        quantity_b= quantity_b+2;
+        displayB(quantity_b);
+    }
+    public void point1B(View view) {
+        quantity_b= quantity_b+1;
+        displayB(quantity_b);
+    }
+    /**
+     * Reset
+     */
+    public void reset(View view) {
+        quantity_b= quantity_b-quantity_b;
+        displayB(quantity_b);
+
+        quantity= quantity-quantity;
+        displayA(quantity);
     }
 }
